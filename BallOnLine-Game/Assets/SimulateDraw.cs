846c6f4c76a93ad15ab1f5f9bc7a8cd7b65d7fdd
myxsan <yksl3461@gmail.com>
2022-08-06 22:39:41 +0300

    Implement Bresenham's line algorithm to draw pixels

--- BallOnLine-Game/Assets/SimulateDraw.cs
+++ BallOnLine-Game/Assets/SimulateDraw.cs
@@ -6,14 +6,17 @@ using UnityEngine.UI;
 public class SimulateDraw : MonoBehaviour
 {
     [SerializeField] GameObject drawPlane;
+    [SerializeField] Color paintColor = Color.white;
+
     Vector2Int lastPoint = Vector2Int.zero;
     float rayDistance;
+    Queue<Vector2Int> drawnPoints = new Queue<Vector2Int>();
 
     Texture2D texture;
     void Start()
     {
         texture = Instantiate(drawPlane.GetComponent<Renderer>().material.mainTexture) as Texture2D;
-        for (int y = 0; y < texture.height; y++) // makes texture black
+        for (int y = 0; y < texture.height; y++) // paint all pixels black
         {
             for (int x = 0; x < texture.width; x++)
             {
@@ -24,26 +27,74 @@ public class SimulateDraw : MonoBehaviour
         texture.Apply();
 
         rayDistance = drawPlane.transform.position.z - Camera.main.transform.position.z + 1;
-        //rayDistance = drawPanel.transform.parent.GetComponentInParent<Canvas>().planeDistance + 1;
     }
     void Update()
     {
+        //record start of mouse drawing to get the first position the mouse touches down
+        if (Input.GetMouseButtonDown(0))
+        {
+            RaycastHit ray;
+            if(Physics.Raycast(Camera.main.ScreenPointToRay(Input.mousePosition), out ray))
+            {
+                lastPoint = new Vector2Int((int)(ray.textureCoord.x * texture.width),
+                                        (int)(ray.textureCoord.y * texture.height));
+            }
+        }
+
+        //draw a line between the last known location of the mouse and the current location
         if (Input.GetMouseButton(0))
         {
-            RaycastHit _ray;
-            if (Physics.Raycast(Camera.main.ScreenPointToRay(Input.mousePosition), out _ray))
+            RaycastHit ray;
+            if (Physics.Raycast(Camera.main.ScreenPointToRay(Input.mousePosition), out ray))
             {
-                lastPoint = new Vector2Int((int)(_ray.textureCoord.x * texture.width),
-                                        (int)(_ray.textureCoord.y * texture.height));
+                DrawPixelLine((int)(ray.textureCoord.x * texture.width),
+                                   (int)(ray.textureCoord.y * texture.height),
+                                   (int)lastPoint.x, (int)lastPoint.y, Color.white, texture);
+
+                lastPoint = new Vector2Int((int)(ray.textureCoord.x * texture.width),
+                                        (int)(ray.textureCoord.y * texture.height));
             }
 
-            if(_ray.collider)
-            {
-                Debug.Log("hey " + _ray.collider.name + " " + lastPoint);
-            }
-
-            texture.SetPixel(lastPoint.x, lastPoint.y, Color.white);
             texture.Apply();
         }
+    }
+
+    //For more information on the algorithm see: https://en.wikipedia.org/wiki/Bresenham%27s_line_algorithm
+    void DrawPixelLine(int x, int y, int x2, int y2, Color color, Texture2D texture)
+    {
+        int w = x2 - x;
+        int h = y2 - y;
+        int dx1 = 0, dy1 = 0, dx2 = 0, dy2 = 0;
+        if (w < 0) dx1 = -1; else if (w > 0) dx1 = 1;
+        if (h < 0) dy1 = -1; else if (h > 0) dy1 = 1;
+        if (w < 0) dx2 = -1; else if (w > 0) dx2 = 1;
+        int longest = Mathf.Abs(w);
+        int shortest = Mathf.Abs(h);
+        if (!(longest > shortest))
+        {
+            longest = Mathf.Abs(h);
+            shortest = Mathf.Abs(w);
+            if (h < 0) dy2 = -1; else if (h > 0) dy2 = 1;
+            dx2 = 0;
+        }
+        int numerator = longest >> 1;
+        for (int i = 0; i <= longest; i++)
+        {
+            texture.SetPixel(x, y, color);
+            drawnPoints.Enqueue(new Vector2Int(x, y));
+            numerator += shortest;
+            if (!(numerator < longest))
+            {
+                numerator -= longest;
+                x += dx1;
+                y += dy1;
+            }
+            else
+            {
+                x += dx2;
+                y += dy2;
+            }
+        }
+        texture.Apply();
     }
 }
